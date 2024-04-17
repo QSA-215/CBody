7a2404c04c3ed69f3f0cfb4a60f43f124c12bb20
QSA-215 <dolgorukov-22122003@mail.ru>
2024-04-17 21:58:53 +0300

    2

--- CBody/main.cpp
+++ CBody/main.cpp
@@ -16,7 +16,6 @@ std::shared_ptr<CBody> GetMostMassBody(const std::vector<std::shared_ptr<CBody>>
 
 std::shared_ptr<CBody> GetLeastMassBodyInWater(const std::vector<std::shared_ptr<CBody>>& bodies)
 {
-	const double G = 9.8;
 	const double WaterDensity = 1000;
 
 	std::shared_ptr<CBody> LessMassBodyInWater = bodies[0];
@@ -34,55 +33,91 @@ void GetInfo(const std::vector<std::shared_ptr<CBody>>& bodies)
 		std::cout << body->ToString() << std::endl;
 };
 
-void AddBody(std::istringstream& lineStream, std::vector<std::shared_ptr<CBody>>& bodies)
+std::shared_ptr<CBody> AddSimpleBody(std::istringstream& lineStream, const std::string& command)
 {
-	std::string command;
-	lineStream >> command;
-	if ((command == "compound") || (command == "}"))
-	{
-		CCompound compoundBody;
-		
-
-
-		bodies.push_back(std::make_shared<CBody>(compoundBody));
-	}
-	else if (command == "cone")
+	if (command == "cone")
 	{
 		double density, height, baseRadius;
 		lineStream >> density >> height >> baseRadius;
-		bodies.push_back(std::make_shared<CBody>(CCone(density, height, baseRadius)));
+		return std::make_shared<CBody>(CCone(density, height, baseRadius));
 	}
 	else if (command == "cylinder")
 	{
 		double density, height, baseRadius;
 		lineStream >> density >> height >> baseRadius;
-		bodies.push_back(std::make_shared<CBody>(CCylinder(density, height, baseRadius)));
+		return std::make_shared<CBody>(CCylinder(density, height, baseRadius));
 	}
 	else if (command == "parallelepiped")
 	{
 		double density, width, height, depth;
 		lineStream >> density >> width >> height >> depth;
-		bodies.push_back(std::make_shared<CBody>(CParallelepiped(density, width, height, depth)));
+		return std::make_shared<CBody>(CParallelepiped(density, width, height, depth));
 	}
 	else if (command == "sphere")
 	{
 		double density, radius;
 		lineStream >> density >> radius;
-		bodies.push_back(std::make_shared<CBody>(CSphere(density, radius)));
+		return std::make_shared<CBody>(CSphere(density, radius));
+	}
+};
+
+std::shared_ptr<CBody> AddCompoundBody(std::istream& input)
+{
+	CCompound compoundBody;
+	std::string line, command;
+	std::istringstream lineStream(line);
+	std::getline(input, line);
+	while (std::getline(input, line))
+	{
+		lineStream.clear();
+		lineStream.str(line);
+		lineStream >> command;
+		if (command == "compound")
+		{
+			compoundBody.AddChildBody(std::make_shared<CBody>(AddCompoundBody(std::cin)));
+		}
+		else if ((command == "cone") || (command == "cylinder") ||
+			(command == "parallelepiped") || (command == "sphere"))
+		{
+			compoundBody.AddChildBody(std::make_shared<CBody>(AddSimpleBody(lineStream, command)));
+		}
+		else if (command == "}")
+		{
+			return std::make_shared<CBody>(compoundBody);
+		}
+		else
+		{
+			std::cout << "error" << std::endl;
+		}
+		command = "";
 	}
 };
 
 int main()
 {
 	std::vector<std::shared_ptr<CBody>> bodies;
-	std::string line;
+	std::string line, command;
 	std::istringstream lineStream(line);
 
 	while (std::getline(std::cin, line))
 	{
 		lineStream.clear();
 		lineStream.str(line);
-		AddBody(lineStream, bodies);
+		lineStream >> command;
+		if (command == "compound")
+		{
+			bodies.push_back(std::make_shared<CBody>(AddCompoundBody(std::cin)));
+		}
+		else if ((command == "cone") || (command == "cylinder") ||
+			(command == "parallelepiped") || (command == "sphere"))
+		{
+			bodies.push_back(std::make_shared<CBody>(AddSimpleBody(lineStream, command)));
+		}
+		else
+		{
+			std::cout << "error" << std::endl;
+		}
+		command = "";
 	}
 	if (bodies.empty())
 		return 0;
